e8b4bf026f40bc18d2ad89f0caf7f0c2a108df9e
grac43 <34543433+grac43@users.noreply.github.com>
2018-08-03 15:16:07 -0400

    dg update

--- mainfile.py
+++ mainfile.py
@@ -3,20 +3,31 @@ Final project psedocode
 -   Connect to Google Direction’s API
 -   Connect to Uber’s API
 
--   current_location = Ask user for their current location
+# send text to user on saturday at 8am
+#are you heading to school today?
+# if yes, then next
 
--   def get_travel_time(current_location, arguments) #Write function to take user’s location & whatever other information is needed and returns: 
-o   subway_time = Subway travel time to destination
-o   driving_time = Driving travel time to destination
+#if no, hell yea.. ejoy your saturday!
+
+-   current_location = Ask user for their current location
+# as if user is at home (yes, no)
+#if home, pull address
+#if not home, ask for address
+
+
+
+# -   def get_travel_time(current_location, arguments) #Write function to take user’s location & whatever other information is needed and returns: 
+# o   subway_time = Subway travel time to destination
+# o   driving_time = Driving travel time to destination
  
--   def get_uber(current_location, arguments) #Writes function to request an uber
-o   Returns a message that somehow confirms you got an uber
+# -   def get_uber(current_location, arguments) #Writes function to request an uber
+# o   Returns a message that somehow confirms you got an uber
  
--   if subway_time > driving_time: #Compare subway travel time and driving travel time. 
-o   If true:
-   Ask the user: do you want an uber (y/n)?:
-•   If y:
-o   get_uber() #run the get uber function to call you an uber
-•   If n: print(“You’re going to be late to school!”)
-o   Else: print(f“Take the subway and save some money. Your expected arrival time at school is {arrival_time}”)
+# -   if subway_time > driving_time: #Compare subway travel time and driving travel time. 
+# o   If true:
+#    Ask the user: do you want an uber (y/n)?:
+# •   If y:
+# o   get_uber() #run the get uber function to call you an uber
+# •   If n: print(“You’re going to be late to school!”)
+# o   Else: print(f“Take the subway and save some money. Your expected arrival time at school is {arrival_time}”)
  
