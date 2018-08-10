c310055bcb8f9c5767623428b6eab4302be8c5bb
prs0107 <42074735+prs0107@users.noreply.github.com>
2018-08-10 10:06:08 -0400

    Add files via upload

---
+++ travel.py
@@ -0,0 +1,62 @@
+import googlemaps
+import geocoder 
+from datetime import datetime
+from datetime import timedelta
+
+#from pytimeparse.timeparse import timeparse - I don't think I need this. Will delete. 
+
+#import all of our own functions from another file
+from travel_functions import get_travel_time, find_transit_mode, are_you_late, print_time, send_excuse
+#ADD THE IMPORTS FOR THE CITIBIKE FUNCTIONS FROM THEIR FILES
+
+gmaps = googlemaps.Client(key='AIzaSyD1zTwme37sTYJy2y5gBzOg9TluuK2xgcc')
+now = datetime.now() #gets current time
+
+current = str(input("Where are you now? ")) + ', NY'
+destination = str(input("Where do you want to go? ")) + ', NY'
+
+current_lat = geocoder.google(current).lat
+current_lng = geocoder.google(current).lng
+dest_lat = geocoder.google(destination).lat
+dest_lng = geocoder.google(destination).lng
+
+print("Ok, I will find out how long it will take you to get there!")
+
+travel_times = get_travel_time(now, current, destination) #run this function to get the users travel times
+
+for i in travel_times:
+    arrival_time = now + timedelta(seconds = travel_times[i])
+    print(f"{i} you will arrive at {arrival_time.strftime('%H:%M')}")
+
+min = min(travel_times.items(),key=lambda x: x[1]) 
+print(f"{min[0]} is fastest.")
+
+user_mode = find_transit_mode() #run function to find out what travel method user is using
+arrival_time = now + timedelta(seconds = travel_times[user_mode])
+
+####ADD SOME CITIBIKE CODE HERE TO SHOW THE CITBIKE INFO AT YOUR CURRENT & DESTINATION####
+
+correct_arrival_time = datetime.strptime(input("When were you supposed to arrive? Enter hh:mm am/pm "), '%I:%M%p')
+correct_arrival_time = correct_arrival_time.replace(year=now.year, month = now.month, day = now.day)
+
+late_time = are_you_late(arrival_time, correct_arrival_time) #run function to determine whether user is late
+
+if arrival_time > correct_arrival_time: 
+#if clause to determine what to do if the user is late
+
+    if input("Do you want to send an excuse text? ").lower == "yes":
+    #if user is late, ask if they want you to text their friend, run another function to do so
+
+        phone_number = input("What is your friend' phone number? ")
+        if (user_mode == 'bicycling'): #A citibike specific excuse
+            excuse = "I'm so sorry I could not get a Citibike, I will be " + print_time(late_time)
+        
+        else:
+            excuse = "I'm so sorry, I will be "
+
+        excuse = excuse + print_time(late_time)
+        send_excuse(phone_number, excuse)
+else:
+    print("Being timely is a virtue, good job!")
+    exit()   
+    #end function if user is on-time
---
+++ travel_functions.py
@@ -0,0 +1,89 @@
+#File containing all the functions our program uses
+import googlemaps
+import geocoder 
+from datetime import datetime
+from datetime import timedelta
+from pytimeparse.timeparse import timeparse
+from urllib.request import urlopen
+import json
+import time
+from math import cos, asin, sqrt
+
+gmaps = googlemaps.Client(key='AIzaSyD1zTwme37sTYJy2y5gBzOg9TluuK2xgcc')
+
+#FUNCTION 1
+def get_travel_time(now, current, destination): #function to get the travel time between destinations
+    #create a dictionary to store values of different travel tmies
+    travel_times_int = {'driving' : 0,
+                    'transit' : 0,
+                    'bicycling' : 0,
+                    'walking': 0,
+                    }
+
+    #Run for loop to cycle through google API to get travel time for different modes of transport
+    for mode in ['transit', 'driving', 'walking', 'bicycling']:
+        directions_result = gmaps.directions(current,
+                                         destination,
+                                         mode,
+                                         departure_time=now)
+        travel_times_int[mode] = directions_result[0]['legs'][0]['duration']['value']
+
+    return travel_times_int #function's output is the dictionary with travel time's as datetime 
+
+#FUNCTION 2
+def find_transit_mode(): #function that asks user for their mode of transport
+    user_input_mode = input("How are you getting there? subway, car, walking, or bike? ")
+
+    if user_input_mode == "subway" or user_input_mode == 'train': 
+        mode = "transit"
+    elif user_input_mode == "bike" or user_input_mode == 'cycle' or user_input_mode == 'biking':
+        mode = 'bicycling'
+    elif user_input_mode == "car" or user_input_mode == 'uber' or user_input_mode == 'taxi':
+        mode = 'driving'
+    elif user_input_mode == "walking":
+        mode = user_input_mode
+    else:
+        print("That is not an accepted transit type. Please re-run program")
+        exit() #breaks if you type something other than what the program can accept
+    return mode
+
+#FUNCTION 3
+def print_time(time): #function to show a time in a readable way 
+    time = str(time) #converts datetime class to string
+    time = time.split(':') #splits the string by colon character
+
+    #if clauses for different formatting options:
+    if time[0] == '0':
+        time = time[1] + " minutes"
+    
+    elif time[0] == '1':
+        time = time[0] + " hr and " + time[1]  + " minutes"
+
+    else:
+        time = time[0] + " hrs and " + time[1]  + " minutes"
+    return time
+
+#FUNCTION 4
+def are_you_late(arrival_time, correct_arrival_time): #function to say whether you will be on-time
+    if arrival_time <= correct_arrival_time: #output for if you're on-time
+        print("You will be on-time!")
+        print(f"You will arrive at: {arrival_time.time().replace(microsecond=0)}")
+        late_time = 0
+
+    else: #function for if you're late. Calls the print_time function
+        late_time = arrival_time - correct_arrival_time
+        print(f"You will be {print_time(late_time)} late!")
+    return late_time
+
+#FUNCTION 5
+def send_excuse(phone_number, excuse): #function that uses Twilio to send a text if you're running late
+    from twilio.rest import Client
+
+    account = "ACce880bdf137e2d193b3deaeadc5ef68f"
+    token = "f2adc22dae86e6b8a173f2e63c7289ef"
+    client = Client(account, token)
+    #from number hard coded to my Twilio
+    message = client.messages.create(to=phone_number, from_="+12015145987",
+                                     body=excuse) 
+
+    print("Your excuse was sent! Now hurry!")
