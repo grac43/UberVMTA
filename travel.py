4ae8e6405bfd8451827e61b66acb8b88534d659c
grac43 <34543433+grac43@users.noreply.github.com>
2018-08-10 18:37:02 -0400

    formatting
    additional updates, mostly formatting

--- travel.py
+++ travel.py
@@ -29,7 +29,7 @@ dest_lat = geocode_destination.latitude
 dest_lng = geocode_destination.longitude
 
 
-print("Ok, I will find out how long it will take you to get there!")
+print("Ok, I will find out how long it will take you to get there!\n")
 
 travel_times = get_travel_time(now, current, destination) #run this function to get the users travel times
 
@@ -38,7 +38,17 @@ for i in travel_times:
     print(f"{i} you will arrive at {arrival_time.strftime('%H:%M')}")
 
 min = min(travel_times.items(),key=lambda x: x[1]) 
-print(f"{min[0]} is fastest.")
+print(f"\n{min[0]} is fastest.\n")
+
+# dan's code current latlng
+closest_origin = citibike.get_closest_station((current_lat, current_lng))
+closest = closest_origin
+print (f"\nThe nearest CitiBike Station based on your current location is on {closest['stAddress1']} \nTotal Open Docks(s):{closest['availableDocks']} \nTotal Available Bike(s): {closest['availableBikes']}\n")
+
+# dan's code destination latlng
+closest_dest   = citibike.get_closest_station((dest_lat, dest_lng))
+closest = closest_dest
+print (f"\nThe nearest CitiBike Station based on your destination is on {closest['stAddress1']} \nTotal Open Docks(s):{closest['availableDocks']} \nTotal Available Bike(s): {closest['availableBikes']}")
 
 user_mode = find_transit_mode() #run function to find out what travel method user is using
 arrival_time = now + timedelta(seconds = travel_times[user_mode])
@@ -46,17 +56,7 @@ arrival_time = now + timedelta(seconds = travel_times[user_mode])
 
 # print(f"({current_lat}, {current_lng}), ({dest_lat}, {dest_lng})")
 
-# dan's code current latlng
-closest_origin = citibike.get_closest_station((current_lat, current_lng))
-closest = closest_origin
-print (f"The nearest CitiBike Station based on your current location is on {closest['stAddress1']} \nTotal Open Docks(s):{closest['availableDocks']} \nTotal Available Bike(s): {closest['availableBikes']}\n")
-
-# dan's code destination latlng
-closest_dest   = citibike.get_closest_station((dest_lat, dest_lng))
-closest = closest_dest
-print (f"The nearest CitiBike Station based on your destination is on {closest['stAddress1']} \nTotal Open Docks(s):{closest['availableDocks']} \nTotal Available Bike(s): {closest['availableBikes']}")
-
-correct_arrival_time = datetime.strptime(input("When were you supposed to arrive? Enter the military time, with no spaces: "), '%H:%M')
+correct_arrival_time = datetime.strptime(input("\nWhen were you supposed to arrive? Enter the military time, with no spaces: "), '%H:%M')
 correct_arrival_time = correct_arrival_time.replace(year=now.year, month = now.month, day = now.day)
 
 late_time = are_you_late(arrival_time, correct_arrival_time) #run function to determine whether user is late
