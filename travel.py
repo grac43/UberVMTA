d91d72bae567379e5dc6366161446f8665e300e6
grac43 <34543433+grac43@users.noreply.github.com>
2018-08-11 16:09:01 -0400

    Add files via upload

--- travel.py
+++ travel.py
@@ -1,18 +1,10 @@
-import googlemaps
 import geocoder 
 from datetime import datetime
 from datetime import timedelta
-import citibike
 from geopy.geocoders import Nominatim
 
-
-
-
-#from pytimeparse.timeparse import timeparse - I don't think I need this. Will delete. 
-
-#import all of our own functions from another file
-from travel_functions import get_travel_time, find_transit_mode, are_you_late, print_time, send_excuse
-#ADD THE IMPORTS FOR THE CITIBIKE FUNCTIONS FROM THEIR FILES
+#import all of our own functions from  the file with functions
+from t_func import get_travel_time, find_transit_mode, are_you_late, print_time, send_excuse, get_closest_station
 
 now = datetime.now() #gets current time
 
@@ -29,7 +21,7 @@ dest_lat = geocode_destination.latitude
 dest_lng = geocode_destination.longitude
 
 
-print("Ok, I will find out how long it will take you to get there!\n")
+print("Ok, I will find out how long it will take you to get there!")
 
 travel_times = get_travel_time(now, current, destination) #run this function to get the users travel times
 
@@ -40,23 +32,30 @@ for i in travel_times:
 min = min(travel_times.items(),key=lambda x: x[1]) 
 print(f"\n{min[0]} is fastest.\n")
 
-# dan's code current latlng
-closest_origin = citibike.get_closest_station((current_lat, current_lng))
-closest = closest_origin
-print (f"\nThe nearest CitiBike Station based on your current location is on {closest['stAddress1']} \nTotal Open Docks(s):{closest['availableDocks']} \nTotal Available Bike(s): {closest['availableBikes']}\n")
+print("Let me check Citibikes, give a few seconds.")
+try:
+    closest_origin = get_closest_station((current_lat, current_lng))
+    print (f"\nThe nearest CitiBike Station based on your current location is on {closest_origin['stAddress1']} \nTotal Available Bike(s): {closest_origin['availableBikes']}\n")
+except:
+    print("Sorry, I couldn't find a station near you now")
+    pass
 
-# dan's code destination latlng
-closest_dest   = citibike.get_closest_station((dest_lat, dest_lng))
-closest = closest_dest
-print (f"\nThe nearest CitiBike Station based on your destination is on {closest['stAddress1']} \nTotal Open Docks(s):{closest['availableDocks']} \nTotal Available Bike(s): {closest['availableBikes']}")
+try:
+    closest_dest   = get_closest_station((dest_lat, dest_lng))
+    print (f"\nThe nearest CitiBike Station based on your destination is on {closest_dest['stAddress1']} \nTotal Open Docks(s):{closest_dest['availableDocks']}")
+except:
+    print("Sorry, I couldn't find a station near your destination")
+    pass
 
 user_mode = find_transit_mode() #run function to find out what travel method user is using
 arrival_time = now + timedelta(seconds = travel_times[user_mode])
 
+try:
+    correct_arrival_time = datetime.strptime(input("When were you supposed to arrive? Example 1:30pm "), '%I:%M%p')
+except:
+    print("Incorrect format! Format should 10:15am")
+    correct_arrival_time = datetime.strptime(input("When were you supposed to arrive? "), '%I:%M%p')
 
-# print(f"({current_lat}, {current_lng}), ({dest_lat}, {dest_lng})")
-
-correct_arrival_time = datetime.strptime(input("\nWhen were you supposed to arrive? Enter the military time, with no spaces: "), '%H:%M')
 correct_arrival_time = correct_arrival_time.replace(year=now.year, month = now.month, day = now.day)
 
 late_time = are_you_late(arrival_time, correct_arrival_time) #run function to determine whether user is late
@@ -64,19 +63,26 @@ late_time = are_you_late(arrival_time, correct_arrival_time) #run function to de
 if arrival_time > correct_arrival_time: 
 #if clause to determine what to do if the user is late
 
-    if input("Do you want to send an excuse text? (input: yes/no):").lower() =="yes":
+    if input("Do you want to send an excuse text? yes / no").lower() =="yes":
             
         #if user is late, ask if they want you to text their friend, run another function to do so
       
-            phone_number = input("What is your friend' phone number? ").lower() 
-            if (user_mode == 'bike'): 
-#A citibike specific excuse
-                excuse = "I'm so sorry I could not get a Citibike, I will be " + print_time(late_time)
+        phone_number = input("What is your friend' phone number? \n Please enter as +12128675309").lower() 
+            
+        if (user_mode == 'bike'): 
+            #A citibike specific excuse
+            excuse = "I'm so sorry I could not get a Citibike, I will be " + print_time(late_time)
         
-            else:
-                excuse = "I'm so sorry, I will be "
-
-excuse = excuse + print_time(late_time)
-send_excuse(phone_number, excuse)
-#     else:
-# print("Being timely is a virtue, good job!")
+        else:
+            excuse = "I'm so sorry, I will be "
+        #WARNING: The texting function in the other file uses Parijat's Twilio information.
+        #To test, please update the from number in the function and use the to number associated with your Twilio account
+        try:
+            excuse = excuse + print_time(late_time)
+            send_excuse(phone_number, excuse)
+        except: 
+            print("I'm sorry, there was an error and I could not send your message. \n Hurry hurry!")
+    else:
+        print("Ok, well hurry up!")
+else:
+    print("Being timely is a virtue, good job!")
--- travel_functions.py
+++ travel_functions.py
@@ -1,4 +1,4 @@
-#File containing all the functions our program uses
+
 import googlemaps
 import geocoder 
 from datetime import datetime
@@ -7,7 +7,9 @@ from pytimeparse.timeparse import timeparse
 from urllib.request import urlopen
 import json
 import time
-from math import cos, asin, sqrt
+import requests
+from operator import itemgetter, attrgetter
+
 
 gmaps = googlemaps.Client(key='AIzaSyD1zTwme37sTYJy2y5gBzOg9TluuK2xgcc')
 
@@ -47,8 +49,8 @@ def find_transit_mode(): #function that asks user for their mode of transport
         exit() #breaks if you type something other than what the program can accept
     return mode
 
-#FUNCTION 3
-def print_time(time): #function to show a time in a readable way 
+#FUNCTION 3:
+def print_time(time): 
     time = str(time) #converts datetime class to string
     time = time.split(':') #splits the string by colon character
 
@@ -76,14 +78,54 @@ def are_you_late(arrival_time, correct_arrival_time): #function to say whether y
     return late_time
 
 #FUNCTION 5
+#WARNING: This texting function uses Parijat's Twilio information.
+#To test, please update the from number in the function and use the to number associated with your Twilio account
 def send_excuse(phone_number, excuse): #function that uses Twilio to send a text if you're running late
     from twilio.rest import Client
 
     account = "ACce880bdf137e2d193b3deaeadc5ef68f"
     token = "f2adc22dae86e6b8a173f2e63c7289ef"
     client = Client(account, token)
-    #from number hard coded to my Twilio
+   #WARNING: CHANGE THIS FROM NUMBER TO WHATEVER IS ASSOCIATED WITH YOUR TWILLIO ACCOUNT TO TEST
     message = client.messages.create(to=phone_number, from_="+12015145987",
                                      body=excuse) 
 
     print("Your excuse was sent! Now hurry!")
+
+#FUNCTION 6
+def get_closest_station(origin, destinations, bucket_sz = 100): #Function that finds the closest citibike to you
+  """
+  :param origin
+  Origin is a single tuple (latitude, longitude)
+  :param destinations
+  Destinations is a list of (latitude, longitude)
+  :param bucket_sz
+  Bucket size when retreiving result. Max should be 100
+  """
+  results = []
+  bucket_sz = 100
+  # api
+  gmaps = googlemaps.Client(key='AIzaSyD1zTwme37sTYJy2y5gBzOg9TluuK2xgcc') #our private key
+
+  #for bucket of lat longs, the program will run an iteration to figure out the total walking time in seconds from current location to citibike terminal
+  for idx in range(0, len(destinations), bucket_sz):
+    latlng_bucket = destinations[idx: idx + bucket_sz];
+    results_bucket = gmaps.distance_matrix(origins = origin, destinations = latlng_bucket, mode = "walking")
+    results.extend(results_bucket['rows'][0]['elements'])
+
+
+  length = len(results) #the total length of our citibike list
+  durations = [] #empty list that will be appended as we run the code
+
+  for index in range(length):
+    # get result
+    result =  results[index]
+
+    # make a tuple with (value, station name, index)
+    # save index to be used later for retrieving more info about station
+    info = (result['duration']['value'], index)
+    # append info to a list
+    durations.append(info)
+
+    sorted_durations = sorted(durations, key=itemgetter(0)) #sorts smallest to larget based on the "first collumn" which is the duration in seconds
+  return sorted_durations[0][1] #pulls out the closest citbike station index
